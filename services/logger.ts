85491c8eea807684ab6ea8a505759ef1ebb28395
ttetrafon <ttetrafon@yahoo.gr>
2026-09-11 15:52:32 +0300

    [services] logger++

--- services/logger.ts
+++ services/logger.ts
@@ -1,10 +1,46 @@
-class Logger {
-  _instance: Logger | undefined = undefined;
+export type LogLevel = 'debug' | 'info' | 'error';
 
-  constructor() {
-    if (!this._instance) {
-      this._instance = this;
+export class Logger {
+  #instance: Logger | undefined = undefined;
+  #logLevel: LogLevel;
+  #appName: string;
+
+  constructor(logLevel: LogLevel, appName: string) {
+    if (!this.#instance) {
+      this.#instance = this;
     }
-    return this._instance;
+
+    this.#logLevel = logLevel;
+    this.#appName = appName;
+    return this.#instance;
+  }
+
+  formatMsg(msg: Error | string, module: string | undefined) {
+    let res: string;
+
+    if (msg instanceof Error) {
+      res = msg.message;
+    }
+    else {
+      res = msg;
+    }
+
+    return `[${new Date().toUTCString()}::${this.#appName}${module ? '::' + module : ''}] res`;
+  }
+
+  async log(msg: string | Error, module: string | undefined = undefined): Promise<void> {
+    if (this.#logLevel === 'error' || this.#logLevel === 'info') return;
+
+    console.log(this.formatMsg(msg, module));
+  }
+
+  async warn(msg: string | Error, module: string | undefined = undefined): Promise<void> {
+    if (this.#logLevel == 'error') return;
+
+    console.warn(this.formatMsg(msg, module));
+  }
+
+  async error(msg: string | Error, module: string | undefined = undefined): Promise<void> {
+    console.log(this.formatMsg(msg, module));
   }
 }
